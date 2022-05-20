d100fb58e7852cbb8d78ab4e7c119ebb8738a68b
Majid Shakeel Shawl <dev2.renosoftwares@gmail.com>
2022-05-21 02:18:25 +0530

    Added initial route with widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_it_done/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Home();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+      },
+    );
   }
 }
