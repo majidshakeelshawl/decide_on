2506b80522ecd3140ec6b5357bcb65a27b562b3b
Majid Shakeel Shawl <majidshakeelshawl@gmail.com>
2022-06-05 02:11:44 +0530

    Added firestore and junkcode to check whether firestore is working

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,17 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class DashBoard extends StatelessWidget {
+import '../widgets/cbutton.dart';
+
+class DashBoard extends StatefulWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
   final _auth = FirebaseAuth.instance;
-  DashBoard({Key? key}) : super(key: key);
-
+  final _firestore = FirebaseFirestore.instance;
+  var currentDocument = null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Text("${_auth.currentUser?.displayName}"),
+        title: Text("Hello"),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          CButton(
+            onTap: () async {
+              try {
+                currentDocument = await _firestore
+                    .collection('users')
+                    .doc(_auth.currentUser?.uid)
+                    .get();
+                print(currentDocument.data());
+                // await _firestore
+                //     .collection('users')
+                //     .doc(_auth.currentUser?.uid)
+                //     .update({"name": "WHO", "country": "Switzerland"});
+                // currentDocument = await _firestore
+                //     .collection('users')
+                //     .doc(_auth.currentUser?.uid)
+                //     .get();
+                // print(currentDocument.data());
+              } catch (error) {
+                print("Error ::: ${error}");
+              }
+            },
+            font: GoogleFonts.akayaTelivigala(),
+            buttonColor: Colors.red,
+            width: 100.3,
+            margin: EdgeInsets.all(10),
+            borderColor: Colors.blue,
+            height: 200.3,
+            text: 'Try Firestore',
+          ),
+          Center(
+            child: Column(
+              children: [
+                Text(""),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
